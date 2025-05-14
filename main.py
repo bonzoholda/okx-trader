7b13584671fd613b72c71fadc8d745b92840c2f0
bonzoholda <bhumibeka@gmail.com>
2025-05-14 20:43:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse, HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from datetime import datetime
 
 
 bot = TradingBot()
