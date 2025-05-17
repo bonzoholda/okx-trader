195f677eb8594fa045851acdfb3cc25a9cc09e82
bonzoholda <bhumibeka@gmail.com>
2025-05-17 18:08:25 +0700

    Update trading.py

--- trading.py
+++ trading.py
@@ -148,9 +148,9 @@ class TradingBot:
                 
 
         # --- Close at trailing TP ---
-        if self.active_position == "long" and price < self.trailing_tp:
+        if self.active_position == "long" and price < self.trailing_tp and change >= TP_THRESHOLD:
             self.close_position("long")
-        elif self.active_position == "short" and price > self.trailing_tp:
+        elif self.active_position == "short" and price > self.trailing_tp and change >= TP_THRESHOLD:
             self.close_position("short")
 
 
